e147e1e394589e45b48917aba799dedaa0466cd8
Sicily Ding <xishading@gmail.com>
2022-12-14 00:21:44 -0700

    stain setup

--- play.js
+++ play.js
@@ -2,5 +2,14 @@ const net = require("net");
 
 const { connect } = require('./client');
 
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    stdin.on("data", key => handleUserInput(key));
+    return stdin;
+  };
+
 console.log("Connecting ...");
 connect();
