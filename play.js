ba12641ab99d0e7191907d51e65800c681502054
Sicily Ding <xishading@gmail.com>
2022-12-14 00:25:04 -0700

    input module

--- play.js
+++ play.js
@@ -1,15 +1,7 @@
-const net = require("net");
-
-const { connect } = require('./client');
-
-const setupInput = function () {
-    const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding("utf8");
-    stdin.resume();
-    stdin.on("data", key => handleUserInput(key));
-    return stdin;
-  };
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
+
+setupInput();
